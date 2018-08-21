510f5690d205ce12f0aa3122ae54e31491a94083
_v0n <mfjvaughan@gmail.com>
2018-08-20 23:20:27 -0400

    made a function

--- update_spreadsheet.py
+++ update_spreadsheet.py
@@ -22,31 +22,42 @@ if not creds or creds.invalid:
 
 service = discovery.build('sheets', 'v4', credentials=creds)
 
-# Test data
-df = pd.read_csv(TEST_DATA)
-df_cols, df_data = df.columns.tolist(), df.values.tolist()
-values = ([df_cols] + df_data)
+def update_gsheet_data(worksheet, data):
+    # Test data
+    if type(data) is list:
+        values = data
+        RANGE_END_CHAR = len(values[0]) # Based on the # of columns of data
+        RANGE_END_INT = len(values[1:]) # Based on the number of rows of data 
+    else:
+        df = pd.read_csv(data)
+        data_cols, data_values = df.columns.tolist(), df.values.tolist()
+        values = ([data_cols] + data_values)
+        RANGE_END_CHAR = col_string(len(data_cols)) # Based on the # of columns of data
+        RANGE_END_INT = len(values) # Based on the number of rows of data
 
-# Some variables ...
-SHEET_NAME = ''
-RANGE_START = 'A1' # Always start at A1
-RANGE_END_CHAR = col_string(len(df_cols)) # Based on the # of columns of data
-RANGE_END_INT = len(values) # Based on the number of rows of data
-VALUE_INPUT_OPTION = 'USER_ENTERED'
+    # Some more variables ...
+    SHEET_NAME = ''
+    RANGE_START = 'A1' # Always start at A1
+    VALUE_INPUT_OPTION = 'USER_ENTERED'
 
-# Build the request ...
-data = [
-    {
-        'values': values,
-        'range': '{}!{}:{}{}'.format(SHEET_NAME, RANGE_START, RANGE_END_CHAR, RANGE_END_INT)
+    # Build the request ...
+    data = [
+        {
+            'values': values,
+            'range': '{}!{}:{}{}'.format(SHEET_NAME, RANGE_START, RANGE_END_CHAR, RANGE_END_INT)
+        }
+    ]
+
+    body = {
+    'valueInputOption': VALUE_INPUT_OPTION,
+    'data': data
     }
-]
 
-body = {
-  'valueInputOption': VALUE_INPUT_OPTION,
-  'data': data
-}
-
-result = service.spreadsheets().values().batchUpdate(spreadsheetId=SPREADSHEET_ID, body=body)
-response = result.execute()
-pprint(response)
+    result = service.spreadsheets().values().batchUpdate(spreadsheetId=SPREADSHEET_ID, body=body)
+    response = result.execute()
+    pprint(response)
+    
+    return response
+    
+if __name__ == "__main__":   
+    update_gsheet_data(SPREADSHEET_ID, TEST_DATA)
